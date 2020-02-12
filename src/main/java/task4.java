7c0b96281a6e8d380521713f19024f77261c02e0
Timur Huseynov <timur.physics@mail.ru>
2020-02-12 21:27:41 +0400

    #array task

---
+++ src/main/java/task4.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class task4 {
+    public static void main(String[] args) {
+        final int LEN = 30;
+        int[] array = new int[LEN];
+        int[] arrayPos = new int[LEN];
+        int[] arrayNeg = new int[LEN];
+
+        int ni = 0;
+        int pi = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*100-50);
+        }
+
+
+        for (int el : array) {
+            if (el > 0){
+                arrayPos[pi++] = el;
+            }
+        }
+
+        for (int el : array) {
+            if (el < 0){
+                arrayNeg[ni++] = el;
+            }
+        }
+
+        int[] neg = Arrays.copyOf(arrayNeg,ni);
+        int[] pos = Arrays.copyOf(arrayPos, pi);
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(pos));
+        System.out.println(Arrays.toString(neg));
+    }
+}
+
