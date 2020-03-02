ee701a39cf78d9bbee62d0fbd1eda8cde8deb820
Timur Huseynov <timur.physics@mail.ru>
2020-03-02 19:45:23 +0400

    initialcommit

---
+++ src/main/java/Tasks/SentenceTask10.java
@@ -0,0 +1,22 @@
+package Tasks;
+
+public class SentenceTask10 {
+    public final String subj;
+    public final String verb;
+    public final String obj;
+
+    public SentenceTask10(String subj, String verb, String obj) {
+        this.subj = subj;
+        this.verb = verb;
+        this.obj = obj;
+    }
+
+    public String represent(){
+        return String.format("%s %s %s", subj,verb,obj);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Sentence[%s]", represent());
+    }
+}
