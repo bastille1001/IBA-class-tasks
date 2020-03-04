9a39eafdc35fe7bff93c2d6467d84751517bb012
Timur Huseynov <timur.physics@mail.ru>
2020-03-04 19:07:52 +0400

    initialcommit

--- src/main/java/Tasks/Task11Sentences.java
+++ src/main/java/Tasks/Task11Sentences.java
@@ -6,37 +6,57 @@ import java.util.stream.Collectors;
 
 public class Task11Sentences {
 
-    private static HashMap<String,List<String>> read(String filename) throws IOException {
+    private static List<String> read(String filename) throws FileNotFoundException {
+        File ifile= new File(filename);
+        FileReader fr= new FileReader(ifile);
+        BufferedReader br= new BufferedReader(fr);
+        return br.lines().collect(Collectors.toList());
+    }
+
+    private static void write(List<String> lines, String filename) throws IOException {
         File file = new File(filename);
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-        List<String> ls = br.lines().collect(Collectors.toList());
-        HashMap<String, List<String>> hm = new HashMap<>();
-
-        for (String line: ls) {
-            String[] ss = line.split(":");
-            String key = ss[0].trim();
-            String[] valueArr = ss[1].trim().split(", ");
-
-            List<String> value = Arrays.asList(valueArr);
-            hm.put(key,value);
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (String line: lines) {
+            bw.write(line);
+            bw.write("\n");
         }
-        br.close();
-        return hm;
+        bw.close();
+    }
+
+    private static List<String > process(Map<String, List<String>> convert1, Map<String, List<String>> convert2) {
+        List<String> sentences= new ArrayList<>();
+        String sentence=null;
+        for (String s:convert1.keySet())
+            for (String v: convert1.get(s))
+                for (String o:convert2.get(v)){
+                    sentence = String.format("%s %s %s ",s,v,o);
+                    sentences.add(sentence);
+                }
+        return sentences;
+    }
+
+    private static Map<String, List<String>> convertLS(List<String> data) {
+        Map<String, List<String>> map = new HashMap<>();
+        Iterator<String> it = data.iterator();
+        while (it.hasNext()){
+            String el = it.next();
+            String[] splitted = el.split("[:,]");
+            List<String> val = new ArrayList<>();
+            for(int i=1;i<splitted.length;i++)
+                val.add(splitted[i]);
+            map.put(splitted[0],val);}
+        return map;
     }
 
 
     public static void main(String[] args) throws IOException {
-        HashMap<String,List<String>> sv = read("subj_verb_.txt");
-        HashMap<String,List<String>> vo = read("verb_obj_.txt");
+        List<String> verb_obj = read("subj_verb_.txt");
+        List<String> subj_obj = read("verb_obj_.txt");
+        Map<String, List<String>> convert1 = convertLS(verb_obj);
+        Map<String, List<String>> convert2 = convertLS(subj_obj);
+        List<String> sentence = process(convert1,convert2);
+        write(sentence,"file6.txt");
 
-        for (String s: sv.keySet()) {
-            for (String v: sv.get(s)) {
-                for (String o: vo.get(v)) {
-                    System.out.println(new Sentence(s,v,o));
-                }
-            }
-
-        }
     }
 }
