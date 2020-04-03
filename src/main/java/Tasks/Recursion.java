c8c504ff099f1498615673ccec2e2f78dfedcf20
bastille1001 <timur.physics@mail.ru>
2020-04-03 09:52:12 +0400

    #updated

---
+++ src/main/java/Tasks/Recursion.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(faktorial(5));
+        System.out.println(fibonacci(32));
+    }
+
+    private static int faktorial(int num) {
+        if (num == 0 || num == 1) return 1;
+        int fakt = faktorial(num - 1);
+        return num * fakt;
+    }
+
+    private static int fibonacci(int num){
+        if (num == 1 || num == 2) return 1;
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
