4f20baca82e44d66056c572f0efd9803245daa8e
bastille1001 <timur.physics@mail.ru>
2020-04-15 21:09:46 +0400

    warm up task

--- src/main/java/lesson1/OOP/Dog.java
+++
@@ -1,9 +0,0 @@
-package lesson1.OOP;
-
-public class Dog extends Animal {
-
-    public Dog(String name){ super(name); }
-
-    @Override
-    public String toString() { return String.format("I`m animal, my name is %s", name); }
-}
